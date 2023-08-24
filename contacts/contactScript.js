6f938ec555903c019bbea4fae2cf44aabe07081f
BuildTools <unconfigured@null.spigotmc.org>
2023-08-24 19:09:54 +0400

    Initial Commit

---
+++ contacts/contactScript.js
@@ -0,0 +1,21 @@
+
+
+function ytClick() {
+    window.location.href = "https://www.youtube.com/@yacinehamza111"
+}
+
+function twitterClick() {
+    window.location.href = "https://twitter.com/yacinehamza1111"
+}
+
+function twitchClick() {
+    window.location.href = "https://www.twitch.tv/yacinehamza111"
+}
+
+function patreonClick() {
+    window.location.href = "https://www.patreon.com/yacinehamza111"
+}
+
+function discordClick() {
+    window.location.href = "https://discord.gg/kjEnSdDuu2"
+}
